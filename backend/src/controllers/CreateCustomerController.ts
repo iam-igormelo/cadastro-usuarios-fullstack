a78b7281f67435f5bdfd3ad1084345264618e90f
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2025-11-04 06:00:02 -0300

    Criação da rota de cadastrar clientes

--- backend/src/controllers/CreateCustomerController.ts
+++ backend/src/controllers/CreateCustomerController.ts
@@ -3,8 +3,10 @@ import { CreateCustomerService } from "../services/CreateCustomerService.js";
 
 class CreateCustomerController {
     async handle(request:FastifyRequest, reply:FastifyReply) {
+        const { name, email } = request.body as { name:string, email:string}
+
         const customerService = new CreateCustomerService()
-        const customer = await customerService.execute()
+        const customer = await customerService.execute({ name, email })
         reply.send(customer)
     }
 }
