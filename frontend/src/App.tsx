800c11e4ca76d076cbef82763a087e06ea7aec91
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2025-11-09 17:02:52 -0300

    Criação do formulário de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function App() {
+  return(
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
-function App() {
-  const [count, setCount] = useState(0)
+        <form className="flex flex-col my-6">
+          <label className="font-medium text-white">Nome:</label>
+          <input 
+            type="text" 
+            placeholder="Digite o seu nome completo..."
+            className="w-full mb-5 p-2 rounded bg-white"
+          />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <label className="font-medium text-white">Email:</label>
+          <input 
+            type="email" 
+            placeholder="Digite o seu email..."
+            className="w-full mb-5 p-2 rounded bg-white"
+          />
+
+          <input 
+            type="submit" 
+            value="Cadastrar" 
+            className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
+          />
+        </form>
+      </main>
+    </div>
   )
 }
-
-export default App
