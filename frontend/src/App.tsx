c57f4cafc648181f885e2f35a57c61a9f9148c33
Igor Albuquerque Melo <93369336+iam-igormelo@users.noreply.github.com>
2025-11-09 19:59:28 -0300

    Finalização do formulário de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import { FiTrash } from "react-icons/fi"
+
 export default function App() {
   return(
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
-      <main className="my-10 w-full md:max-w-2xl">
+      <main className="my-10 w-full max-w-md">
         <h1 className="text-4xl font-medium text-white">Clientes</h1>
 
         <form className="flex flex-col my-6">
@@ -25,6 +27,17 @@ export default function App() {
             className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
           />
         </form>
+
+        <section className="flex flex-col">
+          <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+            <p><span className="font-medium">Nome:</span> Matheus</p>
+            <p><span className="font-medium">Email:</span> matheus@gmail.com</p>
+            <p><span className="font-medium">Status:</span> ATIVO</p>
+            <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
+              <FiTrash size={18} color="#FFF"/>
+            </button>
+          </article>
+        </section>
       </main>
     </div>
   )
